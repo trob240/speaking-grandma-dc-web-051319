20123c253500afc4020c25d4d7b4d05dcae78c65
Trevahr Robertson <trevahr@yahoo.com>
2019-04-17 20:37:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,17 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if 
-    phrase != phrase.upcase
+  
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    
+  elsif  phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   
-  elsif
+  else phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+    
+  end
   
   
 end
